1a4cc849dec23a3d26c4052f2f75bf6419efa783
kuzutech <smashinfries1@gmail.com>
2023-09-22 16:13:53 -0500

    test new style

--- src/features/models/components/card.tsx
+++ src/features/models/components/card.tsx
@@ -1,28 +1,36 @@
 import { View } from "react-native";
 import { Image } from 'expo-image';
-import { Text, MD3DarkTheme } from 'react-native-paper';
+import { Text, MD3DarkTheme, IconButton, useTheme } from 'react-native-paper';
 import { LinearGradient } from "expo-linear-gradient";
 import { CivitAIModelItem } from "../../../app/api/civitai";
 import { useState } from "react";
 import { useSharedValue } from "react-native-reanimated";
+import { MD3Colors } from "react-native-paper/lib/typescript/types";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type ModelCardProps = {
     item: CivitAIModelItem;
+    themeColors: MD3Colors;
     index: number;
 };
-export const ModelCard = ({item}:ModelCardProps) => {
+export const ModelCard = ({item, themeColors}:ModelCardProps) => {
+    
     if (!item) {
         return null;
     }
 
     return(
-        <View style={{margin:8, borderRadius:12}}>
-            <Image source={{uri: item?.modelVersions[0]?.images[0]?.url}} style={{ borderRadius:12, overflow:'hidden', width:150, height:220}} />
-            <LinearGradient colors={['transparent', '#000']} locations={[0.7, 1]} style={{ borderRadius:12, overflow:'hidden', position:'absolute', justifyContent:'flex-end', alignItems:'center', width:150, height:220}}>
-                <View style={{flexDirection:'row',}}>
-                    <Text style={{color:MD3DarkTheme.colors.onSurface}}>{item.stats.downloadCount}</Text>
-                </View>
-            </LinearGradient>
+        <View style={{margin:8,}}>
+            <View>
+                <Image source={{uri: item?.modelVersions[0]?.images[0]?.url}} style={{ borderRadius:12, overflow:'hidden', width:150, height:220}} />
+            </View>
+                {/* <LinearGradient colors={['transparent', '#000']} locations={[0.7, 1]} style={{ borderRadius:12, overflow:'hidden', position:'absolute', justifyContent:'flex-end', alignItems:'center', width:150, height:220}}>
+            
+            </LinearGradient> */}
+            <View style={{position:'absolute', flexDirection:'row', padding:5, borderTopRightRadius:12, borderBottomLeftRadius:12, right:0, top:0, backgroundColor: themeColors.primaryContainer}}>
+                
+                <Text style={{color:themeColors.onPrimaryContainer}}>{item.stats.downloadCount}</Text>
+            </View>
             <Text numberOfLines={2} style={{padding:5, width:150, textAlign:'center'}}>{item.name}</Text>
         </View>
     )
