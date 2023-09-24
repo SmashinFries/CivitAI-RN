65c32c6048316be2fbf624e2e837549f3996b708
Smashinfries <jus10luka@gmail.com>
2023-09-24 03:49:17 -0500

    blur image based on nsfw settings

--- src/components/images/card.tsx
+++ src/components/images/card.tsx
@@ -3,13 +3,16 @@ import { Chip, Text } from "react-native-paper";
 import {Image } from 'expo-image'
 import { LinearGradient } from "expo-linear-gradient";
 import { CivitAIImage, CivitAiNSFW, ModelImagesItem } from "../../api/civitai";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { NSFWTag } from "../labels";
 import { Link, router } from "expo-router";
+import { useSettingsStore } from "../../store";
 
 const blurhash =
   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
+
+const nsfwLevelOrder = [CivitAiNSFW.None, CivitAiNSFW.Soft, CivitAiNSFW.Mature, CivitAiNSFW.X];
 
 type ImageCardProps = {
     item: CivitAIImage;
@@ -19,20 +22,22 @@ type ImageCardProps = {
 };
 export const ImageCard = ({index, maxHeight, width, item}:ImageCardProps) => {
     const [isBlur, setIsBlur] = useState<boolean>(true);
+    const {maxNSFWLevel} = useSettingsStore();
+    const userNsfwLevel = useMemo(() => nsfwLevelOrder.findIndex((value) => value === maxNSFWLevel), [maxNSFWLevel]);
+    const imageNsfwLevel = useMemo(() => nsfwLevelOrder.findIndex((value) => value === item.nsfwLevel), [item.nsfwLevel]);
+
     if (!item) {
         return null;
     }
 
     return(
-        // <Link href={{pathname:'images/[id]', params:{id:item.id}}} asChild>
         <Pressable onPress={() => router.push({pathname:'image/[id]', params:{id:item?.id}})} onLongPress={() => setIsBlur(prev => !prev)} style={{ borderRadius:12}}>
-            <Image source={{uri: item?.url}} placeholder={blurhash} blurRadius={ item.nsfw && isBlur ? 200 : 0} transition={800} contentFit="cover" style={{ aspectRatio:item?.width/item?.height, borderRadius:12, width:width-8, maxHeight:maxHeight,}} />
+            <Image source={{uri: item?.url}} placeholder={blurhash} blurRadius={ item.nsfw && userNsfwLevel < imageNsfwLevel && isBlur ? 200 : 0} transition={800} contentFit="cover" style={{ aspectRatio:item?.width/item?.height, borderRadius:12, width:width-8, maxHeight:maxHeight,}} />
             {/* <LinearGradient colors={['transparent', '#000']} locations={[0.7, 1]} style={{ aspectRatio:item?.width/item?.height, width:width-8, maxHeight:maxHeight, borderRadius:12, overflow:'hidden', position:'absolute', justifyContent:'flex-end'}}>
                 <Text numberOfLines={2} style={{padding:5, color:MD3DarkTheme.colors.onSurface, textAlign:'center'}}>{item.}</Text>
             </LinearGradient> */}
             <NSFWTag nsfw={item.nsfwLevel} />
         </Pressable>
-        // </Link>
     )
 }
 
