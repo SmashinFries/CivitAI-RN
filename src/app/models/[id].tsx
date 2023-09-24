6c3fdf3bb9150663f59987d98e3a8948fb32908b
Smashinfries <jus10luka@gmail.com>
2023-09-23 19:39:29 -0500

    update model header title

--- src/app/models/[id].tsx
+++ src/app/models/[id].tsx
@@ -1,21 +1,27 @@
 import { ScrollView, View, useWindowDimensions } from "react-native";
-import { useImageIdQuery, useImagesQuery, useModelQuery } from "../../api/api";
+import { useModelQuery } from "../../api/api";
 import { Image } from "expo-image";
-import { useLocalSearchParams } from "expo-router";
-import { ActivityIndicator } from "react-native-paper";
+import { Stack, useLocalSearchParams } from "expo-router";
 import { RefreshControl } from "react-native-gesture-handler";
 
 const ModelDetails = () => {
-    const windowSize = useWindowDimensions();
     const { id } = useLocalSearchParams<{ id: string }>();
     const {data, isFetching, refetch, isRefetching} = useModelQuery(id);
 
     return(
-        <ScrollView style={{flex:1}} refreshControl={<RefreshControl refreshing={isRefetching} onRefresh={refetch} />}>
-            {/* {data?.items[0] ? <View>
-                
-            </View> : <ActivityIndicator size={'large'} />} */}
-        </ScrollView>
+        <>
+            <Stack.Screen
+                options={{
+                title: data?.name ?? ''
+                }}
+            />
+            <ScrollView style={{flex:1}} refreshControl={<RefreshControl refreshing={isRefetching} onRefresh={refetch} />}>
+                {/* {data?.items[0] ? <View>
+                    
+                </View> : <ActivityIndicator size={'large'} />} */}
+            </ScrollView>
+        </>
+        
     )
 }
 
