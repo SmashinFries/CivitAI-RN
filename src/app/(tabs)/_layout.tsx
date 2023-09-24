f2c7e84909ae882d906e868bdd1751642ec2fb46
Smashinfries <jus10luka@gmail.com>
2023-09-24 15:13:29 -0500

    add material bottom tabs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,17 +1,28 @@
-import { Tabs } from "expo-router";
+import { Tabs, withLayoutContext } from "expo-router";
 // @ts-ignore
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {
+	createMaterialBottomTabNavigator,
+	MaterialBottomTabNavigationOptions,
+  } from "@react-navigation/material-bottom-tabs";
+  
+const { Navigator } = createMaterialBottomTabNavigator();
 
+// Copied from https://github.com/EvanBacon/expo-router-layouts-example/blob/main/layouts/material-bottom-tabs.tsx
+// Not sure how to properly type this
+export const MaterialBottomTabs = withLayoutContext<
+	MaterialBottomTabNavigationOptions,
+	typeof Navigator
+>(Navigator);
 
 const TabLayout = () => {
     return(
-        <Tabs initialRouteName='models' screenOptions={props => ({tabBarStyle:{height:56}, headerShown:false})}>
-					<Tabs.Screen name="models" options={{title:'Models', tabBarIcon:props => <Icon {...props} name='database-outline' />}} />
-					<Tabs.Screen name="images" options={{title:'Images', tabBarIcon:props => <Icon {...props} name='image-multiple-outline' />}} />
-					<Tabs.Screen name="creators" options={{href: null, title:'Creators', tabBarIcon:props => <Icon {...props} name='account-outline' />}} />
-					<Tabs.Screen name="more" options={{title:'More', tabBarIcon:props => <Icon {...props} name='dots-horizontal' />}} />
-					{/* <Tabs.Screen name="index" options={{href: null}} /> */}
-				</Tabs>
+        <MaterialBottomTabs initialRouteName='models'>
+			<MaterialBottomTabs.Screen name="models" options={{title:'Models', tabBarIcon: 'database-outline'}} />
+			<MaterialBottomTabs.Screen name="images" options={{title:'Images', tabBarIcon: 'image-multiple-outline'}} />
+			<MaterialBottomTabs.Screen name="creators" options={{ title:'Creators', tabBarIcon:'account-outline'}} />
+			<MaterialBottomTabs.Screen name="more" options={{title:'More', tabBarIcon: 'dots-horizontal'}} />
+		</MaterialBottomTabs>
     );
 };
 
