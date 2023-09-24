484ce192f7180e2fe4389af8a9d4d78059b3ef5d
Smashinfries <jus10luka@gmail.com>
2023-09-23 19:17:14 -0500

    add img transition

--- src/components/images/card.tsx
+++ src/components/images/card.tsx
@@ -17,7 +17,7 @@ export const ImageCard = ({index, maxHeight, width, item}:ImageCardProps) => {
 
     return(
         <View style={{ borderRadius:12}}>
-            <Image source={{uri: item?.url}} contentFit="cover" style={{ aspectRatio:item?.width/item?.height, borderRadius:12, width:width-8, maxHeight:maxHeight,}} />
+            <Image source={{uri: item?.url}} transition={800} contentFit="cover" style={{ aspectRatio:item?.width/item?.height, borderRadius:12, width:width-8, maxHeight:maxHeight,}} />
             <LinearGradient colors={['transparent', '#000']} locations={[0.7, 1]} style={{ aspectRatio:item?.width/item?.height, width:width-8, maxHeight:maxHeight, borderRadius:12, overflow:'hidden', position:'absolute', justifyContent:'flex-end'}}>
                 {/* <Text numberOfLines={2} style={{padding:5, color:MD3DarkTheme.colors.onSurface, textAlign:'center'}}>{item.}</Text> */}
             </LinearGradient>
