81869df2a6347f221f35e88a9aaf03a6c771ef82
Smashinfries <jus10luka@gmail.com>
2023-09-23 19:16:54 -0500

    add refresh control

--- src/app/models/[id].tsx
+++ src/app/models/[id].tsx
@@ -1,15 +1,17 @@
 import { ScrollView, View, useWindowDimensions } from "react-native";
-import { useImageIdQuery, useImagesQuery } from "../../api/api";
+import { useImageIdQuery, useImagesQuery, useModelQuery } from "../../api/api";
 import { Image } from "expo-image";
 import { useLocalSearchParams } from "expo-router";
 import { ActivityIndicator } from "react-native-paper";
+import { RefreshControl } from "react-native-gesture-handler";
 
 const ModelDetails = () => {
     const windowSize = useWindowDimensions();
     const { id } = useLocalSearchParams<{ id: string }>();
+    const {data, isFetching, refetch, isRefetching} = useModelQuery(id);
 
     return(
-        <ScrollView style={{flex:1}}>
+        <ScrollView style={{flex:1}} refreshControl={<RefreshControl refreshing={isRefetching} onRefresh={refetch} />}>
             {/* {data?.items[0] ? <View>
                 
             </View> : <ActivityIndicator size={'large'} />} */}
