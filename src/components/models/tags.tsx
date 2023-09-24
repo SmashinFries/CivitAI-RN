ba500f9a3ce61b2c2689576b4261cd7bbdcdb44d
Smashinfries <jus10luka@gmail.com>
2023-09-23 21:44:22 -0500

    add model tags

---
+++ src/components/models/tags.tsx
@@ -0,0 +1,19 @@
+import { Chip } from "react-native-paper";
+import { View } from "react-native";
+
+export const ModelTag = (tag:string) => {
+    return(
+        <Chip>{tag}</Chip>
+    );
+};
+
+type ModelVersionTagProps = {
+    name: string;
+    isSelected?: boolean;
+    onPress: () => void;
+};
+export const ModelVersionTag = ({name, isSelected, onPress}:ModelVersionTagProps) => {
+    return(
+            <Chip onPress={onPress} selected={isSelected}>{name}</Chip>
+    );
+};
