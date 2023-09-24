f4e14944bb39a199db96f2c9452e302e7949380c
Smashinfries <jus10luka@gmail.com>
2023-09-24 15:14:18 -0500

    update creators

--- src/app/(tabs)/creators/_layout.tsx
+++ src/app/(tabs)/creators/_layout.tsx
@@ -1,8 +1,12 @@
 import { Stack } from "expo-router";
+import { PaperHeader } from "../../../components/headers";
 
 const CreatorsLayout = () => {
     return(
-        <Stack />
+        <Stack initialRouteName="index" screenOptions={{headerShown:true, header:(props) => <PaperHeader {...props} />}}>
+            <Stack.Screen name='index' options={{title:'Creators'}} />
+            {/* <Stack.Screen name='[id]' options={{title:'Details', header:(props) => <PaperHeader showIcon={false} {...props} />}} /> */}
+        </Stack>
     );
 };
 
--- src/app/(tabs)/creators/index.tsx
+++ src/app/(tabs)/creators/index.tsx
@@ -1,7 +1,12 @@
+import { View } from 'react-native';
 import { Text } from 'react-native-paper';
 
 const CreatorsPage = () => {
-  return <Text>Home page</Text>;
+  return(
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text>Creators coming soon!</Text>
+    </View>
+  );
 }
 
 export default CreatorsPage;
