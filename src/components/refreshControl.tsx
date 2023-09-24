6134913a120bc8ced9d50898e8ed73835cb2750e
Smashinfries <jus10luka@gmail.com>
2023-09-24 04:20:27 -0500

    add themed refresh control component

---
+++ src/components/refreshControl.tsx
@@ -0,0 +1,7 @@
+import { RefreshControl, RefreshControlProps } from "react-native";
+import { useTheme } from "react-native-paper";
+
+export const ThemedRefreshControl = (props:RefreshControlProps) => {
+    const { colors } = useTheme();
+    return <RefreshControl {...props} progressBackgroundColor={colors.primaryContainer} tintColor={colors.onPrimaryContainer} />
+};
