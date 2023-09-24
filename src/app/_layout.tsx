1a0141b972e78b440c6d18421ce06adc4b0b4022
Smashinfries <jus10luka@gmail.com>
2023-09-24 02:58:55 -0500

    rerender app on theme change

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,26 @@
 import { Navigator, Slot, Stack, Tabs, } from 'expo-router';
-import { BottomNavigation, MD3LightTheme, PaperProvider, adaptNavigationTheme } from 'react-native-paper';
+import { BottomNavigation, MD3DarkTheme, MD3LightTheme, PaperProvider, adaptNavigationTheme } from 'react-native-paper';
 // @ts-ignore
 import { ThemeProvider, DarkTheme as NavDarkTheme, DefaultTheme, TabRouter } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { useThemeStore } from '../store';
 
 const { LightTheme, DarkTheme } = adaptNavigationTheme({ reactNavigationLight: DefaultTheme, reactNavigationDark: NavDarkTheme });
 
 const queryClient = new QueryClient();
 
 const RootLayout = () => {
-  return(
-    <PaperProvider theme={MD3LightTheme}>
-      	<ThemeProvider value={LightTheme}>
-			<QueryClientProvider client={queryClient}>
-				<Stack screenOptions={{animation:'slide_from_bottom', headerShown:false}} />
-			</QueryClientProvider>
-			<StatusBar style="auto" />
-    	</ThemeProvider>
-    </PaperProvider>
-    
+	const {darkMode} = useThemeStore();
+	return(
+		<PaperProvider theme={darkMode ? MD3DarkTheme : MD3LightTheme}>
+			<ThemeProvider value={darkMode ? DarkTheme : LightTheme}>
+				<QueryClientProvider client={queryClient}>
+					<Stack screenOptions={{animation:'slide_from_bottom', headerShown:false}} />
+				</QueryClientProvider>
+				<StatusBar style={darkMode ? 'light' : 'dark'} />
+			</ThemeProvider>
+		</PaperProvider>
   );
 };
 
