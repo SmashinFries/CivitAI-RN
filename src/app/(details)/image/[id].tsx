62775dee955a358bd67e22ad06e61eec78eb0ada
Smashinfries <jus10luka@gmail.com>
2023-09-25 04:40:56 -0500

    handle new images

--- src/app/(details)/image/[id].tsx
+++ src/app/(details)/image/[id].tsx
@@ -1,26 +1,34 @@
 import { Pressable, ScrollView, View, useWindowDimensions } from "react-native";
-import { useImageIdQuery, useImagesQuery } from "../../../api/api";
+import { useImageIdQuery } from "../../../api/api";
 import { Image } from "expo-image";
 import { useLocalSearchParams } from "expo-router";
-import { ActivityIndicator } from "react-native-paper";
 import { InteractionBar } from "../../../components/images/interaction";
 import { StatsBar } from "../../../components/images/stats";
 import { MetaField } from "../../../components/images/meta";
 import { ThemedRefreshControl } from "../../../components/refreshControl";
-import { useSettingsStore } from "../../../store";
-import { useEffect, useState } from "react";
 import { useNsfwBlur } from "../../../hooks/useNsfwBlur";
+import { LoadingIcon } from "../../../components/loading";
+import { useEffect } from "react";
+import { Text } from "react-native-paper";
 
 const ImageDetails = () => {
     const windowSize = useWindowDimensions();
     const { id } = useLocalSearchParams<{ id: string }>();
-    const {data, isFetching, isRefetching, refetch} = useImageIdQuery({imageId:id});
+    const {data, isFetching, isFetched, isRefetching, isError, error, refetch} = useImageIdQuery({imageId:id});
 
     const {blurAmount, toggleBlur } = useNsfwBlur(data?.items[0]?.nsfwLevel);
 
+    if (isFetched && !data?.items[0]) {
+        return(
+            <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                <Text>Image details not available yet 🥲</Text>
+            </View>
+        );
+    }
+
     return(
-        <ScrollView style={{flex:1}} refreshControl={<ThemedRefreshControl refreshing={isRefetching} onRefresh={refetch} />}>
-            {data?.items[0] ? <Pressable onLongPress={toggleBlur}>
+        <ScrollView contentContainerStyle={{flex:1}} refreshControl={<ThemedRefreshControl refreshing={isRefetching} onRefresh={refetch} />}>
+            {!isFetching && data?.items ? <Pressable onLongPress={toggleBlur}>
                 <Image source={{uri: data?.items[0]?.url}} blurRadius={blurAmount} transition={800} style={{aspectRatio:data?.items[0]?.width/data?.items[0]?.height, alignSelf:'center', width: windowSize.width, maxHeight: windowSize.height/1.5}} />
                 <InteractionBar id={data?.items[0]?.id} image_url={data?.items[0]?.url} share_url={`https://civitai.com/images/${data?.items[0]?.id}`} />
                 <StatsBar {...data?.items[0]?.stats} />
@@ -29,11 +37,10 @@ const ImageDetails = () => {
                 <MetaField label="Resolution" value={data?.items[0]?.meta?.Size} />
                 <MetaField label="Prompt" value={data?.items[0]?.meta?.prompt} />
                 <MetaField label="Negative Prompt" value={data?.items[0]?.meta?.negativePrompt} />
-                
                 <MetaField label="Sampler" value={data?.items[0]?.meta?.sampler} />
                 <MetaField label="CFG Scale" value={data?.items[0]?.meta?.cfgScale} />
                 <MetaField label="Clip Skip" value={data?.items[0]?.meta && data?.items[0]?.meta["Clip skip"]} />
-            </Pressable> : <ActivityIndicator size={'large'} />}
+            </Pressable> : <LoadingIcon />}
         </ScrollView>
     )
 }
