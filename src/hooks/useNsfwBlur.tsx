789a6bc939646304c21afee8f281165c714a7a83
Smashinfries <jus10luka@gmail.com>
2023-09-25 03:50:07 -0500

    add image blur hook

---
+++ src/hooks/useNsfwBlur.tsx
@@ -0,0 +1,19 @@
+import { useMemo, useState } from "react";
+import { useSettingsStore } from "../store";
+import { CivitAiNSFW } from "../api/civitai";
+
+const nsfwLevelOrder = [CivitAiNSFW.None, CivitAiNSFW.Soft, CivitAiNSFW.Mature, CivitAiNSFW.X];
+
+export const useNsfwBlur = (nsfwLevel:CivitAiNSFW|undefined) => {
+    const [isBlur, setIsBlur] = useState<boolean>(true);
+    const {maxNSFWLevel} = useSettingsStore();
+
+    const userNsfwLevel = useMemo(() => nsfwLevelOrder.findIndex((value) => value === maxNSFWLevel), [maxNSFWLevel]);
+    const imageNsfwLevel = useMemo(() => nsfwLevelOrder.findIndex((value) => value === nsfwLevel), [nsfwLevel]);
+
+    const blurAmount = useMemo(() => userNsfwLevel < imageNsfwLevel && isBlur ? 200 : 0, [userNsfwLevel, imageNsfwLevel, isBlur]);
+
+    const toggleBlur = () => setIsBlur(prev => !prev);
+
+    return { blurAmount, toggleBlur}
+};
