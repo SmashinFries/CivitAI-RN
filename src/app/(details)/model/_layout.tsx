4a46bea8bcb1ec10441028d43cfd9e1c74d68781
Smashinfries <jus10luka@gmail.com>
2023-09-26 04:49:32 -0500

    adjust stack style

--- src/app/(details)/model/_layout.tsx
+++ src/app/(details)/model/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 import { PaperHeader } from "../../../components/headers";
 
 const ModelLayout = () => {
-    return <Stack screenOptions={{animation:'slide_from_bottom', title:'', header: props => <PaperHeader {...props} showIcon={false} />}} />
+    return <Stack screenOptions={{animation:'slide_from_bottom', headerShown: false, header: props => <PaperHeader {...props} showIcon={false} />}} />
 };
 
 export default ModelLayout;
